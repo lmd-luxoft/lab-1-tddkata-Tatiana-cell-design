bba9f4fb664da13dd29d62e1056ee635b0a6238b
Tatiana Khodataeva <tatiana.khodataeva@dxc.com>
2021-05-12 11:01:51 +0300

    my second build+test

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -6,9 +6,47 @@ namespace TDDKata
 {
     internal class StringCalc
     {
+
         internal int Sum(string v)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();ArgumentOutOfRangeException}
+            //  ArrayList list = new ArrayList();
+
+            string[] s = v.Split(',');
+            int k = 0;
+            int arg1 = 0;
+            int arg2 = 0;
+            
+
+            foreach (var arg0 in s)
+            {
+               // list.Add(arg0.Trim());
+                string arg3 = arg0.Trim();
+
+                Console.WriteLine(arg3);
+                k++;
+                if (k == 1) {
+                    bool res1 = int.TryParse(arg3, out arg1);
+                    if ((!res1) || (arg1 > 2) || (arg1 < 0)) {
+                        throw new Exception("Неверное значение первого аргумента");
+                    }   
+                }
+                if (k == 2)
+                    {
+                    bool res2 = int.TryParse(arg3, out arg2);
+                    if ((!res2) || (arg2 > 2) || (arg2 < 0)) 
+                       {
+                        throw new Exception("Неверное значение второго аргумента");
+                        }
+                }
+                if (k == 3)
+                    {
+                    throw new Exception("Ошибка. Количество аргументов в строке >= 3"); 
+                     }
+
+            }
+
+            return arg1 + arg2;
         }
     }
 }
--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -4,32 +4,34 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 
+
+
 namespace TDDKata
 {
     [TestFixture]
     public class TestClass
     {
-    /*   [Test]
-        public void SimpleTest()
-        {
-            StringCalc calc = new StringCalc();
-            int value = calc.Sum("2,2");
-            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
-        }
-    */
-        [Test1]
+         [Test]
+            public void SimpleTest()
+         {
+                StringCalc calc = new StringCalc();
+                int value = calc.Sum("2,2");
+                Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+         }
+        
+        [Test]
         public void SimpleTest1()
         {
             //Arrange
             StringCalc calc = new StringCalc();
-  
+
             //Act
             int value = calc.Sum("-2,2");
             //Assert
             Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
 
-        [Test2]
+        [Test]
         public void SimpleTest2()
         {
             //Arrange
@@ -41,7 +43,7 @@ namespace TDDKata
             Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
 
-        [Test3]
+        [Test]
         public void SimpleTest3()
         {
             //Arrange
@@ -53,7 +55,7 @@ namespace TDDKata
             Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
 
-        [Test4]
+        [Test]
         public void SimpleTest4()
         {
             //Arrange
@@ -65,7 +67,7 @@ namespace TDDKata
             Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
 
-        [Test5]
+        [Test]
         public void SimpleTest5()
         {
             //Arrange
@@ -78,4 +80,5 @@ namespace TDDKata
         }
     }
 }
-}
+
+
